6dd2d0fd97ea4f4c37ddffc9ff7dc914e3d86b78
Sandip <sandipm02@icloud.com>
2024-11-18 17:40:21 -0700

    Added Dotenv package & updated code to use config/env

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+import * as path from 'path';
+
+// Load environment variables based on the environment
+ // have to set this on EC2 console like: export NODE_ENV=production
+if (process.env.NODE_ENV === 'production') {
+  dotenv.config({ path: path.join(__dirname, '../config/.env.prod') });
+} else {
+  dotenv.config({ path: path.join(__dirname, '../config/.env.local') });
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.port ?? 3001);
+
+  // Get the port from the environment variable (default to 3001 if not set)
+  const port = process.env.PORT;
+
+  // Start the app on the configured port
+  await app.listen(port);
+  
+  console.log(`Application is running on: http://localhost:${port}`);
 }
+
 bootstrap();
